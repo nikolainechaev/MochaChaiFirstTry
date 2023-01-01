48329fb408aba4cf22b6d54ca42e3e455f4ed6d2
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2023-01-01 16:35:58 -0500

    added couple more tests

--- problems.js
+++ problems.js
@@ -5,3 +5,17 @@
 function getCelcius(farenheit) {
     return farenheit.map(value => (value - 32) * 5/9)
 }
+function addNumbers(numbers){
+    return numbers.reduce((a,b) => a + b)
+}
+function goodOrBadIdea(arr){
+    let goodCounter = 0;
+
+    for(const idea of arr){
+        if(idea === "good"){
+            goodCounter += 1;
+        }
+    }
+    return goodCounter === 0 ? "Fail!"
+    : goodCounter > 2 ? "I smell a series!" : "Publish";
+}
--- tests.js
+++ tests.js
@@ -6,3 +6,17 @@ describe('getCelcius', () =>{
         expect(getCelcius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
     })
 })
+
+describe('addNumbers',() => {
+    it('should add two numbers', () => {
+        expect(addNumbers([2,3])).to.deep.equal(5);
+    })
+})
+
+describe('goodOrBadIdea', () =>{
+    it('should count number of good ideas in array and provide final decision based on number', () =>{
+        expect(goodOrBadIdea(["good", "bad", "good", "bad", "good",])).to.deep.equal("I smell a series!");
+        expect(goodOrBadIdea(["bad", "bad"])).to.deep.equal("Fail!");  
+        expect(goodOrBadIdea(["good","bad"])).to.deep.equal("Publish");
+    })
+})
